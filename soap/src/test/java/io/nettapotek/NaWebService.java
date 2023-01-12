511faeb34b23a61593fe07a43f07e95f21737a65
Markus Haugsdal <markus.haugsdal@sonat.no>
2023-01-12 16:42:24 +0100

    div

--- soap/src/test/java/io/nettapotek/NaWebService.java
+++ soap/src/test/java/io/nettapotek/NaWebService.java
@@ -1,10 +1,23 @@
 package io.nettapotek;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.PlainObject;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jwt.JWTParser;
+import com.nimbusds.jwt.PlainJWT;
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.oauth2.sdk.GeneralException;
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
 import net.shibboleth.utilities.java.support.xml.ParserPool;
 import no.ergo.reseptformidleren.webservices.na.*;
 import no.kith.xmlstds.eresept.m9na1._2016_06_06.M9NA1;
 import no.kith.xmlstds.eresept.m9na2._2016_10_26.M9NA2;
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead;
+import org.apache.cxf.rs.security.jose.jwt.JwtToken;
 import org.opensaml.core.config.InitializationService;
 import org.opensaml.core.xml.XMLObject;
 import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
@@ -13,15 +26,25 @@ import org.opensaml.core.xml.io.UnmarshallerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.annotation.Resource;
+import javax.ws.rs.core.Context;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import javax.xml.ws.WebServiceContext;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Base64;
 import java.util.UUID;
 
 public class NaWebService implements NAWeb {
+
+    @Resource
+    WebServiceContext context;
+
     @Override
     public AppRec naWebServiceVerify(MV parameters) {
         return new AppRec();
@@ -29,6 +52,39 @@ public class NaWebService implements NAWeb {
 
     @Override
     public M9Na2 naWebServiceM9Na1(M9Na1 parameters) throws AppRecFault_Exception {
+
+        var token = (String) context.getMessageContext().get("auth");
+        try {
+            var jwt = JWTParser.parse(token);
+            var header = jwt.getHeader();
+            var claims = jwt.getJWTClaimsSet();
+            System.out.println(header);
+            System.out.println(claims);
+
+            var certsEndpoint = "http://localhost:8080/realms/realm1/protocol/openid-connect/certs";
+            Issuer issuer = new Issuer("http://localhost:8080/realms/realm1");
+            OIDCProviderMetadata opMetadata = OIDCProviderMetadata.resolve(
+                    issuer,
+                    1000,
+                    1000);
+
+            var uri = opMetadata.getJWKSetURI();
+            var json = stream(uri.toURL());
+            var jwkSet = JWKSet.parse(json);
+            var keys = jwkSet.getKeys();
+
+            var rsaKey = keys.get(1).toRSAKey().toRSAPublicKey();
+            var verifier = new RSASSAVerifier(rsaKey);
+            var signedJwt = SignedJWT.parse(token);
+
+            signedJwt.verify(verifier);
+            System.out.println("Verified!");
+
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        } catch (IOException | GeneralException | JOSEException e) {
+            throw new RuntimeException(e);
+        }
 
         var byteDokument = (byte[]) parameters.getDokument();
         try {
@@ -114,5 +170,20 @@ public class NaWebService implements NAWeb {
             throw new Exception("Failed to unmarshal");
         }
         return unmarshaller.unmarshall(samlElement);
+    }
+
+    public static String stream(URL url) {
+        try (InputStream input = url.openStream()) {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            return json.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- soap/src/test/java/io/nettapotek/SoapClientTest.java
+++ soap/src/test/java/io/nettapotek/SoapClientTest.java
@@ -1,27 +1,51 @@
 package io.nettapotek;
 
+import com.nimbusds.oauth2.sdk.*;
+import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
+import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.State;
+import com.nimbusds.openid.connect.sdk.*;
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
+import com.nimbusds.openid.connect.sdk.rp.OIDCClientInformation;
+import com.nimbusds.openid.connect.sdk.rp.OIDCClientInformationResponse;
+import io.nettapotek.interceptor.AuthInterceptor;
 import no.ergo.reseptformidleren.webservices.na.*;
+import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.feature.Feature;
 import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.wss4j.common.ext.WSSecurityException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.ws.rs.core.HttpHeaders;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 public class SoapClientTest {
 
     NAWeb client;
+
+    Client httpClient;
 
     @BeforeClass
     void setup() throws WSSecurityException {
@@ -33,7 +57,7 @@ public class SoapClientTest {
         var loggingFeature = new LoggingFeature();
         features.add(loggingFeature);
         factoryBean.setFeatures(features);
-
+        factoryBean.getInInterceptors().add(new AuthInterceptor());
         WSUtils.setupWSSEServer(factoryBean);
         factoryBean.create();
 
@@ -42,13 +66,22 @@ public class SoapClientTest {
         jaxWsClientFactoryBean.setAddress("http://localhost:8888/NA");
         WSUtils.setupWSSEClient(jaxWsClientFactoryBean);
         client = (NAWeb) jaxWsClientFactoryBean.create();
+        httpClient = ClientProxy.getClient(client);
+
+        //var jwt = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfcVJDUTVQSjJ0NlNHWHZkVnNmQ0YwQU9lSEpsLXNWYVhDOGZBMU4yaHo4In0.eyJleHAiOjE2NzM1MzIyMTUsImlhdCI6MTY3MzUzMTkxNSwiYXV0aF90aW1lIjoxNjczNTMxNTkwLCJqdGkiOiI5MzkyZWM0MS05NGQ0LTQxMzUtOGJmMS0yNTM3MjExYjQzZmYiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvcmVhbG1zL3JlYWxtMSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI2OTZmMjc2OC0xMjQwLTRjMWQtODhhNi1jYzk3YTE0MTVkNDYiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJjbGllbnQxIiwic2Vzc2lvbl9zdGF0ZSI6ImI5OWE4NmEyLTNkY2QtNDQzMi1iZGVkLThlNjJmMWI1M2E5ZiIsImFjciI6IjAiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4ODg4LyJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXJlYWxtMSIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsInNpZCI6ImI5OWE4NmEyLTNkY2QtNDQzMi1iZGVkLThlNjJmMWI1M2E5ZiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoidXNlcjEiLCJnaXZlbl9uYW1lIjoiIiwiZmFtaWx5X25hbWUiOiIifQ.d-NyFn_fgKuzVSLDgErbsaTM3n01YZJlHIqDC2Vsih4SadjfTNjW2WBmFynQwgn5lyen3i96fAXu5teqNbmezYqx9qpe_ugMuS8Ut-gbvQynIVvXfUuQXh3zm0XCcVnmSu_fz_Lc2ThmyDIrK04E2qyKp0Cqt4iNPBQTGfRxL9fGB1j7CZ6XL89dhObqXLaShZ2xjp69lUWa6PUradennNpqMDGOMlfL-eZsa5JbJw31UQ_Ht2VK4QErnM6dfmiwyJMveLq7AupzdxN0OCuL78g0fxZAXCyyPUxG6CZ0OIFVy3s2vVQt9mJuNJQaJ6AvCvZSXYQVLUw869cWRPUiTQ";
+        //httpClient.getRequestContext().put(AUTHORIZATION + ":", "Bearer" + jwt);
+
     }
 
     @Test
     void testOKResponse() throws Exception {
 
+        var jwt = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfcVJDUTVQSjJ0NlNHWHZkVnNmQ0YwQU9lSEpsLXNWYVhDOGZBMU4yaHo4In0.eyJleHAiOjE2NzM1MzIyMTUsImlhdCI6MTY3MzUzMTkxNSwiYXV0aF90aW1lIjoxNjczNTMxNTkwLCJqdGkiOiI5MzkyZWM0MS05NGQ0LTQxMzUtOGJmMS0yNTM3MjExYjQzZmYiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvcmVhbG1zL3JlYWxtMSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI2OTZmMjc2OC0xMjQwLTRjMWQtODhhNi1jYzk3YTE0MTVkNDYiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJjbGllbnQxIiwic2Vzc2lvbl9zdGF0ZSI6ImI5OWE4NmEyLTNkY2QtNDQzMi1iZGVkLThlNjJmMWI1M2E5ZiIsImFjciI6IjAiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4ODg4LyJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXJlYWxtMSIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsInNpZCI6ImI5OWE4NmEyLTNkY2QtNDQzMi1iZGVkLThlNjJmMWI1M2E5ZiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoidXNlcjEiLCJnaXZlbl9uYW1lIjoiIiwiZmFtaWx5X25hbWUiOiIifQ.d-NyFn_fgKuzVSLDgErbsaTM3n01YZJlHIqDC2Vsih4SadjfTNjW2WBmFynQwgn5lyen3i96fAXu5teqNbmezYqx9qpe_ugMuS8Ut-gbvQynIVvXfUuQXh3zm0XCcVnmSu_fz_Lc2ThmyDIrK04E2qyKp0Cqt4iNPBQTGfRxL9fGB1j7CZ6XL89dhObqXLaShZ2xjp69lUWa6PUradennNpqMDGOMlfL-eZsa5JbJw31UQ_Ht2VK4QErnM6dfmiwyJMveLq7AupzdxN0OCuL78g0fxZAXCyyPUxG6CZ0OIFVy3s2vVQt9mJuNJQaJ6AvCvZSXYQVLUw869cWRPUiTQ";
+        Map<String, List<String>> headers = new HashMap<>();
+        headers.put(HttpHeaders.AUTHORIZATION, List.of("Bearer " + jwt));
+        httpClient.getRequestContext().put(Message.PROTOCOL_HEADERS, headers);
+
         M9Na1 m9Na1 = new M9Na1();
-        //m9Na1.setDokument("<m9na1>test</m9na1>".getBytes(StandardCharsets.UTF_8));
         M9na m9na = new M9na();
         m9Na1.setDokument(m9na.getM9na1().getBytes(StandardCharsets.UTF_8));
         try {
