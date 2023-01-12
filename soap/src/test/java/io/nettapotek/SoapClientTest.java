09b2489a6562ade3f22294977b349b5b580dcd88
Markus Haugsdal <markus.haugsdal@sonat.no>
2023-01-12 16:51:08 +0100

    fiks på test

--- soap/src/test/java/io/nettapotek/SoapClientTest.java
+++ soap/src/test/java/io/nettapotek/SoapClientTest.java
@@ -1,17 +1,8 @@
 package io.nettapotek;
 
-import com.nimbusds.oauth2.sdk.*;
-import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
-import com.nimbusds.oauth2.sdk.auth.Secret;
-import com.nimbusds.oauth2.sdk.http.HTTPResponse;
-import com.nimbusds.oauth2.sdk.id.ClientID;
-import com.nimbusds.oauth2.sdk.id.State;
-import com.nimbusds.openid.connect.sdk.*;
-import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
-import com.nimbusds.openid.connect.sdk.rp.OIDCClientInformation;
-import com.nimbusds.openid.connect.sdk.rp.OIDCClientInformationResponse;
 import io.nettapotek.interceptor.AuthInterceptor;
 import no.ergo.reseptformidleren.webservices.na.*;
+import no.kith.xmlstds.eresept.m9na2._2016_10_26.M9NA2;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.feature.Feature;
 import org.apache.cxf.feature.LoggingFeature;
@@ -19,7 +10,6 @@ import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 import org.apache.cxf.message.Message;
-import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.wss4j.common.ext.WSSecurityException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -28,17 +18,14 @@ import org.testng.annotations.Test;
 import javax.ws.rs.core.HttpHeaders;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.handler.MessageContext;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 import java.io.StringReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class SoapClientTest {
@@ -86,21 +73,26 @@ public class SoapClientTest {
         m9Na1.setDokument(m9na.getM9na1().getBytes(StandardCharsets.UTF_8));
         try {
             var response = client.naWebServiceM9Na1(m9Na1);
-
+            var jaxbContext = JAXBContext.newInstance(M9NA2.class);
+            var byteDokument = (byte[]) response.getDokument();
+            var m9Na2 = (M9NA2) jaxbContext.createUnmarshaller().unmarshal(new ByteArrayInputStream(byteDokument));
+            var antall = m9Na2.getAntall(); //TODO m9na2 skal egentlig være fagmelding i en hodemelding
+            Assert.assertEquals(antall, 0);
         } catch (AppRecFault_Exception e) {
-            var apprecDocumentBytes = (byte[]) e.getFaultInfo().getDokument();
-            var jaxbContext = JAXBContext.newInstance(AppRec.class);
-            var um = jaxbContext.createUnmarshaller();
-
-
-            throw new RuntimeException(e);
+            Assert.fail();
         }
     }
 
     @Test
     void testNegativeResponse() throws JAXBException {
 
-        M9Na3 m9Na3= new M9Na3();
+        var jwt = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfcVJDUTVQSjJ0NlNHWHZkVnNmQ0YwQU9lSEpsLXNWYVhDOGZBMU4yaHo4In0.eyJleHAiOjE2NzM1MzIyMTUsImlhdCI6MTY3MzUzMTkxNSwiYXV0aF90aW1lIjoxNjczNTMxNTkwLCJqdGkiOiI5MzkyZWM0MS05NGQ0LTQxMzUtOGJmMS0yNTM3MjExYjQzZmYiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvcmVhbG1zL3JlYWxtMSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI2OTZmMjc2OC0xMjQwLTRjMWQtODhhNi1jYzk3YTE0MTVkNDYiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJjbGllbnQxIiwic2Vzc2lvbl9zdGF0ZSI6ImI5OWE4NmEyLTNkY2QtNDQzMi1iZGVkLThlNjJmMWI1M2E5ZiIsImFjciI6IjAiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4ODg4LyJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXJlYWxtMSIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZSIsInNpZCI6ImI5OWE4NmEyLTNkY2QtNDQzMi1iZGVkLThlNjJmMWI1M2E5ZiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoidXNlcjEiLCJnaXZlbl9uYW1lIjoiIiwiZmFtaWx5X25hbWUiOiIifQ.d-NyFn_fgKuzVSLDgErbsaTM3n01YZJlHIqDC2Vsih4SadjfTNjW2WBmFynQwgn5lyen3i96fAXu5teqNbmezYqx9qpe_ugMuS8Ut-gbvQynIVvXfUuQXh3zm0XCcVnmSu_fz_Lc2ThmyDIrK04E2qyKp0Cqt4iNPBQTGfRxL9fGB1j7CZ6XL89dhObqXLaShZ2xjp69lUWa6PUradennNpqMDGOMlfL-eZsa5JbJw31UQ_Ht2VK4QErnM6dfmiwyJMveLq7AupzdxN0OCuL78g0fxZAXCyyPUxG6CZ0OIFVy3s2vVQt9mJuNJQaJ6AvCvZSXYQVLUw869cWRPUiTQ";
+        Map<String, List<String>> headers = new HashMap<>();
+        headers.put(HttpHeaders.AUTHORIZATION, List.of("Bearer " + jwt));
+        httpClient.getRequestContext().put(Message.PROTOCOL_HEADERS, headers);
+
+
+        M9Na3 m9Na3 = new M9Na3();
         m9Na3.setDokument("<m9na3>test</m9na3>".getBytes(StandardCharsets.UTF_8));
         try {
             var response = client.naWebServiceM9Na3(m9Na3);
