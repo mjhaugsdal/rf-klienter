dde874b301fe7fe53cb20ece8d66e617eff881da
Markus Haugsdal <markus.haugsdal@sonat.no>
2023-02-26 14:46:39 +0100

    migrering

--- message-builder/src/main/java/io/nettapotek/kith/M9na.java
+++ message-builder/src/main/java/io/nettapotek/kith/M9na.java
@@ -1,10 +1,10 @@
 package io.nettapotek.kith;
 
 
+import jakarta.xml.bind.JAXBContext;
 import no.kith.xmlstds.eresept.m9na1._2016_06_06.M9NA1;
 import no.kith.xmlstds.msghead._2006_05_24.MsgHead;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -63,28 +63,28 @@ public class M9na {
         var is = M9na.class.getClassLoader().getResourceAsStream("sarepta-eksempler/ERM9NA1");
         var msgHead = (MsgHead) um.unmarshal(is);
 
-        msgHead.getMsgInfo().setMsgId(UUID.randomUUID().toString());
-        msgHead.getMsgInfo().setGenDate(getXMLGregorianCalendarNow());
+        //msgHead.getMsgInfo().setMsgId(UUID.randomUUID().toString());
+        //msgHead.getMsgInfo().setGenDate(getXMLGregorianCalendarNow());
 
         var properties = new Properties();
         properties.load(M9na.class.getClassLoader().getResourceAsStream("sarepta-eksempler/nettapotek.properties"));
 
         for (var ident : msgHead.getMsgInfo().getSender().getOrganisation().getIdent()) {
             switch (ident.getTypeId().getV()) {
-                case "ENH" -> ident.setId(properties.getProperty("orgnr"));
-                case "LOK" -> ident.setId(properties.getProperty("lok"));
-                case "AKO" -> ident.setId(properties.getProperty("ako"));
-                case "HER" -> ident.setId(properties.getProperty("her"));
+                //case "ENH" -> ident.setId(properties.getProperty("orgnr"));
+                //case "LOK" -> ident.setId(properties.getProperty("lok"));
+                //case "AKO" -> ident.setId(properties.getProperty("ako"));
+                //case "HER" -> ident.setId(properties.getProperty("her"));
             }
         }
         for (var ident : msgHead.getMsgInfo().getPatient().getIdent()) {
             if (ident.getTypeId().getV().equals("FNR") || ident.getTypeId().getV().equals("DNR")) {
-                ident.setId(properties.getProperty("identNr"));
+               // ident.setId(properties.getProperty("identNr"));
             }
         }
 
         var fagmelding = (M9NA1) msgHead.getDocument().get(0).getRefDoc().getContent().getAny().get(0);
-        fagmelding.getIdKunde().setId(properties.getProperty("idKunde"));
+        //fagmelding.getIdKunde().setId(properties.getProperty("idKunde"));
 
 
 
