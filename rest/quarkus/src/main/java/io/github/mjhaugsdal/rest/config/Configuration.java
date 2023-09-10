dedf9a0b60a9a64b54b37892a28f9c7886b02eba
Markus Haugsdal <markus.haugsdal@sonat.no>
2023-09-10 18:44:53 +0200

    mer flytting

---
+++ rest/quarkus/src/main/java/io/github/mjhaugsdal/rest/config/Configuration.java
@@ -0,0 +1,9 @@
+package io.github.mjhaugsdal.rest.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import no.ergo.reseptformidleren.webservices.utleverer.UtlevererWeb;
+
+public class Configuration {
+
+}
