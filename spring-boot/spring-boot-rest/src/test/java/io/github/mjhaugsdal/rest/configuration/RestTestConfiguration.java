ea312df1e5c2b314de84bac4813e2d3177ce5ae5
Markus Haugsdal <markus.haugsdal@sonat.no>
2024-08-18 11:41:21 +0200

    Added config to run towards mock

--- spring-boot/spring-boot-rest/src/test/java/io/github/mjhaugsdal/rest/configuration/RestTestConfiguration.java
+++ spring-boot/spring-boot-rest/src/test/java/io/github/mjhaugsdal/rest/configuration/RestTestConfiguration.java
@@ -26,133 +26,140 @@ import java.util.Properties;
 @Profile("default")
 public class RestTestConfiguration {
 
-    static {
-        System.setProperty("rest.na.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort() + "/v2");
-        System.setProperty("rest.rekvirent.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort()+ "/v2");
-        System.setProperty("rest.utleverer.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort()+ "/v2");
-    }
-
-    @Value("${rest.na.address}")
-    String naAddress;
-
-    @Value("${rest.rekvirent.address}")
-    String rekvirentAddress;
-
-    @Value("${rest.utleverer.address}")
-    String utlevererAddress;
-
-    @Value("${use.jwk}")
-    String jwk;
-
-
-    @Bean
-    public Server naServer() {
-        var serverFactoryBean = new JAXRSServerFactoryBean();
-        serverFactoryBean.setServiceClass(NaWebServiceImpl.class);
-        serverFactoryBean.setAddress(naAddress + "/" + "NA");
-        commonSetup(serverFactoryBean, jwk);
-        return serverFactoryBean.create();
-    }
-
-    @Bean
-    public Server utlevererServer() {
-        var serverFactoryBean = new JAXRSServerFactoryBean();
-        serverFactoryBean.setServiceClass(UtlevererWebServiceImpl.class);
-        serverFactoryBean.setAddress(utlevererAddress + "/" + "Utleverer");
-        commonSetup(serverFactoryBean, jwk);
-        return serverFactoryBean.create();
-    }
-
-
-    @Bean
-    public Server rekvirentServer() {
-        var serverFactoryBean = new JAXRSServerFactoryBean();
-        serverFactoryBean.setServiceClass(RekvirentWebServiceImpl.class);
-        serverFactoryBean.setAddress(rekvirentAddress + "/" + "Rekvirent");
-        commonSetup(serverFactoryBean, jwk);
-        return serverFactoryBean.create();
-    }
-
-    private static void commonSetup(JAXRSServerFactoryBean serverFactoryBean, String jwk) {
-        var loggingFeature = new LoggingFeature();
-        loggingFeature.setPrettyLogging(true);
-        serverFactoryBean.getFeatures().add(loggingFeature);
-
-        List<Object> providers = new LinkedList<>();
-        providers.add("com.fasterxml.jackson.jaxrs.xml.JacksonJaxbXMLProvider");
-
-        var jweContainerRequestFilter = new JweContainerRequestFilter();
-        var jwsContainerRequestFilter = new JwsContainerRequestFilter();
-
-        Properties properties = new Properties();
-        try {
-            properties.load(RestTestConfiguration.class.getClassLoader().getResourceAsStream("server/server.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    @TestConfiguration
+    @Profile("default")
+    public static class DefaultConfiguration {
+        static {
+            System.setProperty("rest.na.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort() + "/v2");
+            System.setProperty("rest.rekvirent.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort()+ "/v2");
+            System.setProperty("rest.utleverer.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort()+ "/v2");
         }
 
-        var jweWriterInterceptor = new JweWriterInterceptor();
-        jweWriterInterceptor.setUseJweOutputStream(true);
+        @Value("${rest.na.address}")
+        String naAddress;
 
-        var jwsWriterInterceptor = new JwsWriterInterceptor();
-        jwsWriterInterceptor.setUseJwsOutputStream(true);
+        @Value("${rest.rekvirent.address}")
+        String rekvirentAddress;
 
-        providers.add(jweContainerRequestFilter);
-        providers.add(jwsContainerRequestFilter);
-        providers.add(jweWriterInterceptor);
-        providers.add(jwsWriterInterceptor);
+        @Value("${rest.utleverer.address}")
+        String utlevererAddress;
 
-        serverFactoryBean.setProviders(providers);
+        @Value("${use.jwk}")
+        String jwk;
 
-        if (Boolean.parseBoolean(jwk)) {
-            //ENCRYPTION properties
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.encryption.in.properties",
-                    "server/jwk/server.properties"
-            );
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.encryption.out.properties",
-                    "server/jwk/server-out.properties"
-            );
-            //SIGNATURE IN
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.signature.in.properties",
-                    "server/jwk/server-sign.properties"
-            );
-            //SIGNATURE OUT
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.signature.out.properties",
-                    "server/jwk/server-out-sign.properties"
-            );
 
-            serverFactoryBean.getProperties(true).put("rs.security.accept.public.key", "true");
-            serverFactoryBean.getProperties(true).put("rs.security.signature.include.public.key", "true");
-        } else {
-            //ENCRYPTION properties
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.encryption.in.properties",
-                    "server/server.properties"
-            );
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.encryption.out.properties",
-                    "server/server-out.properties"
-            );
-            //SIGNATURE IN
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.signature.in.properties",
-                    "server/server-sign.properties"
-            );
-            //SIGNATURE OUT
-            serverFactoryBean.getProperties(true).put(
-                    "rs.security.signature.out.properties",
-                    "server/server-out-sign.properties"
-            );
-            serverFactoryBean.getProperties(true).put("rs.security.signature.include.cert", "true");
+        @Bean
+        public Server naServer() {
+            var serverFactoryBean = new JAXRSServerFactoryBean();
+            serverFactoryBean.setServiceClass(NaWebServiceImpl.class);
+            serverFactoryBean.setAddress(naAddress + "/" + "NA");
+            commonSetup(serverFactoryBean, jwk);
+            return serverFactoryBean.create();
         }
 
-        serverFactoryBean.getProperties(true).put("jose.debug", true);
+        @Bean
+        public Server utlevererServer() {
+            var serverFactoryBean = new JAXRSServerFactoryBean();
+            serverFactoryBean.setServiceClass(UtlevererWebServiceImpl.class);
+            serverFactoryBean.setAddress(utlevererAddress + "/" + "Utleverer");
+            commonSetup(serverFactoryBean, jwk);
+            return serverFactoryBean.create();
+        }
+
+
+        @Bean
+        public Server rekvirentServer() {
+            var serverFactoryBean = new JAXRSServerFactoryBean();
+            serverFactoryBean.setServiceClass(RekvirentWebServiceImpl.class);
+            serverFactoryBean.setAddress(rekvirentAddress + "/" + "Rekvirent");
+            commonSetup(serverFactoryBean, jwk);
+            return serverFactoryBean.create();
+        }
+
+        private static void commonSetup(JAXRSServerFactoryBean serverFactoryBean, String jwk) {
+            var loggingFeature = new LoggingFeature();
+            loggingFeature.setPrettyLogging(true);
+            serverFactoryBean.getFeatures().add(loggingFeature);
+
+            List<Object> providers = new LinkedList<>();
+            providers.add("com.fasterxml.jackson.jaxrs.xml.JacksonJaxbXMLProvider");
+
+            var jweContainerRequestFilter = new JweContainerRequestFilter();
+            var jwsContainerRequestFilter = new JwsContainerRequestFilter();
+
+            Properties properties = new Properties();
+            try {
+                properties.load(RestTestConfiguration.class.getClassLoader().getResourceAsStream("server/server.properties"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            var jweWriterInterceptor = new JweWriterInterceptor();
+            jweWriterInterceptor.setUseJweOutputStream(true);
+
+            var jwsWriterInterceptor = new JwsWriterInterceptor();
+            jwsWriterInterceptor.setUseJwsOutputStream(true);
+
+            providers.add(jweContainerRequestFilter);
+            providers.add(jwsContainerRequestFilter);
+            providers.add(jweWriterInterceptor);
+            providers.add(jwsWriterInterceptor);
+
+            serverFactoryBean.setProviders(providers);
+
+            if (Boolean.parseBoolean(jwk)) {
+                //ENCRYPTION properties
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.encryption.in.properties",
+                        "server/jwk/server.properties"
+                );
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.encryption.out.properties",
+                        "server/jwk/server-out.properties"
+                );
+                //SIGNATURE IN
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.signature.in.properties",
+                        "server/jwk/server-sign.properties"
+                );
+                //SIGNATURE OUT
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.signature.out.properties",
+                        "server/jwk/server-out-sign.properties"
+                );
+
+                serverFactoryBean.getProperties(true).put("rs.security.accept.public.key", "true");
+                serverFactoryBean.getProperties(true).put("rs.security.signature.include.public.key", "true");
+            } else {
+                //ENCRYPTION properties
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.encryption.in.properties",
+                        "server/server.properties"
+                );
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.encryption.out.properties",
+                        "server/server-out.properties"
+                );
+                //SIGNATURE IN
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.signature.in.properties",
+                        "server/server-sign.properties"
+                );
+                //SIGNATURE OUT
+                serverFactoryBean.getProperties(true).put(
+                        "rs.security.signature.out.properties",
+                        "server/server-out-sign.properties"
+                );
+                serverFactoryBean.getProperties(true).put("rs.security.signature.include.cert", "true");
+            }
+
+            serverFactoryBean.getProperties(true).put("jose.debug", true);
+
+        }
+    }
+    @TestConfiguration
+    @Profile("local")
+    public static class LocalConfiguration {
 
     }
-
-
 }
--- spring-boot/spring-boot-soap/src/test/java/io/github/mjhaugsdal/soap/configuration/SoapTestConfiguration.java
+++ spring-boot/spring-boot-soap/src/test/java/io/github/mjhaugsdal/soap/configuration/SoapTestConfiguration.java
@@ -20,77 +20,93 @@ import static io.github.mjhaugsdal.soap.WSTestUtils.setupWSSEServer;
 
 
 @TestConfiguration
-@Profile("default")
 public class SoapTestConfiguration {
 
-    static {
-        System.setProperty("rekvirent.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort() + "/v1");
-        System.setProperty("utleverer.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort()+ "/v1");
-        System.setProperty("na.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort()+ "/v1");
+    @TestConfiguration
+    @Profile("default")
+    public static class DefaultConfiguration {
+        @Value("${rekvirent.sign}")
+        boolean rekvirentSign;
+
+        @Value("${rekvirent.encrypt}")
+        boolean rekvirentEncrypt;
+
+        @Value("${rekvirent.address}")
+        String rekvirentAddress;
+
+        @Value("${utleverer.sign}")
+        boolean utlevererSign;
+
+        @Value("${utleverer.encrypt}")
+        boolean utlevererEncrypt;
+
+        @Value("${utleverer.address}")
+        String utlevererAddress;
+
+        @Value("${na.sign}")
+        boolean naSign;
+
+        @Value("${na.encrypt}")
+        boolean naEncrypt;
+
+        @Value("${na.address}")
+        String naAddress;
+        List<Feature> featureList = new ArrayList<>();
+
+        DefaultConfiguration() {
+            var loggingFeature = new LoggingFeature();
+            loggingFeature.setPrettyLogging(true);
+            featureList.add(loggingFeature);
+        }
+
+        static {
+            System.setProperty("rekvirent.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort() + "/v1");
+            System.setProperty("utleverer.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort() + "/v1");
+            System.setProperty("na.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort() + "/v1");
+            System.setProperty("rest.server.address", "http://localhost:" + TestSocketUtils.findAvailableTcpPort());
+        }
+
+        @Bean
+        public Server naEndpoint() {
+            var bean = new JaxWsServerFactoryBean();
+            bean.setServiceBean(new NaWebService());
+            bean.setAddress(naAddress + "/NA");
+            bean.setFeatures(featureList);
+            setupWSSEServer(bean, naEncrypt, naSign);
+            return bean.create();
+        }
+
+        @Bean
+        public Server rekvirentEndpoint() {
+            var bean = new JaxWsServerFactoryBean();
+            bean.setServiceBean(new RekvirentWebservice());
+            bean.setAddress(rekvirentAddress + "/Rekvirent");
+            bean.setFeatures(featureList);
+            setupWSSEServer(bean, rekvirentEncrypt, rekvirentSign);
+            return bean.create();
+        }
+
+        @Bean
+        public Server utlevererEndpoint() {
+            var bean = new JaxWsServerFactoryBean();
+            bean.setServiceBean(new UtlevererWebservice());
+            bean.setAddress(utlevererAddress + "/Utleverer");
+            bean.setFeatures(featureList);
+            setupWSSEServer(bean, utlevererEncrypt, utlevererSign);
+            return bean.create();
+        }
+
     }
 
-    @Value("${rekvirent.sign}")
-    boolean rekvirentSign;
+    @TestConfiguration
+    @Profile("local")
+    public static class LocalConfiguration {
+        List<Feature> featureList = new ArrayList<>();
 
-    @Value("${rekvirent.encrypt}")
-    boolean rekvirentEncrypt;
-
-    @Value("${rekvirent.address}")
-    String rekvirentAddress;
-
-    @Value("${utleverer.sign}")
-    boolean utlevererSign;
-
-    @Value("${utleverer.encrypt}")
-    boolean utlevererEncrypt;
-
-    @Value("${utleverer.address}")
-    String utlevererAddress;
-
-    @Value("${na.sign}")
-    boolean naSign;
-
-    @Value("${na.encrypt}")
-    boolean naEncrypt;
-
-    @Value("${na.address}")
-    String naAddress;
-    List<Feature> featureList = new ArrayList<>();
-
-    SoapTestConfiguration() {
-        var loggingFeature = new LoggingFeature();
-        loggingFeature.setPrettyLogging(true);
-        featureList.add(loggingFeature);
-    }
-
-
-    @Bean
-    public Server naEndpoint() {
-        var bean = new JaxWsServerFactoryBean();
-        bean.setServiceBean(new NaWebService());
-        bean.setAddress(naAddress + "/NA");
-        bean.setFeatures(featureList);
-        setupWSSEServer(bean, naEncrypt, naSign);
-        return bean.create();
-    }
-
-    @Bean
-    public Server rekvirentEndpoint() {
-        var bean = new JaxWsServerFactoryBean();
-        bean.setServiceBean(new RekvirentWebservice());
-        bean.setAddress(rekvirentAddress + "/Rekvirent");
-        bean.setFeatures(featureList);
-        setupWSSEServer(bean, rekvirentEncrypt, rekvirentSign);
-        return bean.create();
-    }
-
-    @Bean
-    public Server utlevererEndpoint() {
-        var bean = new JaxWsServerFactoryBean();
-        bean.setServiceBean(new UtlevererWebservice());
-        bean.setAddress(utlevererAddress + "/Utleverer");
-        bean.setFeatures(featureList);
-        setupWSSEServer(bean, utlevererEncrypt, utlevererSign);
-        return bean.create();
+        LocalConfiguration() {
+            var loggingFeature = new LoggingFeature();
+            loggingFeature.setPrettyLogging(true);
+            featureList.add(loggingFeature);
+        }
     }
 }
